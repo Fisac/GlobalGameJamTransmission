de77d7193197fffdc26098660e85f5df68440d66
Jacob <jacob.romano@metry.io>
2018-01-26 22:57:38 +0100

    First music manager steps

---
+++ GGJTransmission/Assets/_Scripts/MusicManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioMixer mAudioMixer;
+
+	public void Awake () {
+		float fl;
+		mAudioMixer.GetFloat("MusicVolume", out fl);
+	}
+
+}
