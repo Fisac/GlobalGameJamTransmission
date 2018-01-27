40b3fe4a5c7f6d535b1c8ada516f785236ea8272
GlennFetty6 <glimma85@hotmail.com>
2018-01-27 15:03:37 +0100

    Energyloot2x

---
+++ GGJTransmission/Assets/_Scripts/EnergyLootPlayer1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class EnergyLootPlayer1 : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Player 1")
+        {
+            collision.gameObject.GetComponent<PlayerEnergyInventory>().EnergyAmount += 50;
+            Destroy(gameObject);
+        }
+
+        else if (collision.gameObject.name == "Player 2")
+        {
+            GameObject.Find("GameManager").GetComponent<PlayerEnergyController>().Energy -= 50;
+            Destroy(gameObject);
+        }
+    }
+}
