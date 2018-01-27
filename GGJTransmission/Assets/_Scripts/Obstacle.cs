7942e65465aa67456f6ec6b9e35e604f4aa405c6
Fisac <isac_degerborg@hotmail.com>
2018-01-27 14:31:19 +0100

    Done with particles

--- GGJTransmission/Assets/_Scripts/Obstacle.cs
+++ GGJTransmission/Assets/_Scripts/Obstacle.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour {
-	
-	void Update () {
+
+    [SerializeField] private GameObject particles;
+
+    private void Start() {
+    }
+
+    void Update () {
 	    if(transform.position.y <= -15 || transform.position.y >= 15 || transform.position.x <= -25 || transform.position.x >= 25) {
             Destroy(this.gameObject);
         }
@@ -12,7 +17,8 @@ public class Obstacle : MonoBehaviour {
 
     private void OnCollisionEnter(Collision collision) {
         if(collision.gameObject.tag == "Obstacle") {
-            
+            Instantiate(particles, new Vector3(
+                collision.contacts[0].point.x, collision.contacts[0].point.y, collision.contacts[0].point.z), Quaternion.Euler(0,0,0));
         }
     }
 }
