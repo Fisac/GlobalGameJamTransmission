bae0384a31bf3f4f2a5be7822d40bb10c5affc6d
Fisac <isac_degerborg@hotmail.com>
2018-01-27 14:07:00 +0100

    Added particles

---
+++ GGJTransmission/Assets/_Scripts/Obstacle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+	
+	void Update () {
+	    if(transform.position.y <= -15 || transform.position.y >= 15 || transform.position.x <= -25 || transform.position.x >= 25) {
+            Destroy(this.gameObject);
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.tag == "Obstacle") {
+            
+        }
+    }
+}
