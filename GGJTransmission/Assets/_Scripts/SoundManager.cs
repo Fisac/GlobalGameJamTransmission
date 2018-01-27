b6906d213af29ad4d8e8bd5bcbdb1e827e9e5816
Jacob <jacob.romano@metry.io>
2018-01-27 17:06:03 +0100

    oads

---
+++ GGJTransmission/Assets/_Scripts/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour {
+
+	[SerializeField]
+	AudioSource[] mAudioSources;
+
+	void Start () {
+		
+	}
+
+	public void playPickupEnergy() {
+		mAudioSources [0].Play ();
+	}
+
+	public void playHitByAsteroid() {
+		mAudioSources [1].Play ();
+	}
+
+	public void swooshVelocity() {
+		
+	}
+}
