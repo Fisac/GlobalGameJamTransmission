3a6bfb2d8930ea93d9323574c197041168b67786
Jacob <jacob.romano@metry.io>
2018-01-28 13:01:35 +0100

    merged sounds

---
+++ GGJTransmission/Assets/_Scripts/SoundManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioSource[] mAudioSources;
+
+	public void playAsteroidHit () {
+		mAudioSources[1].Play();
+	}
+
+	public void playEnergyPickup () {
+		Debug.Log ("Energy pickup");
+		mAudioSources [0].Play ();
+	}
+
+	public void playEnergyTransfer () {
+		mAudioSources [2].Play ();
+	}
+
+}
